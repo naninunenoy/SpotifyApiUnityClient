dc7f07ffb16550ad7ee763180ed09a45a26f1008
Nakano Yosuke <ynakano@moff.mobi>
2021-05-07 14:58:39 +0900

    Update Environment.cs

--- Assets/SpotifyApi/Environment.cs
+++ Assets/SpotifyApi/Environment.cs
@@ -1,4 +1,4 @@
-﻿namespace SpotifyApi {
+namespace n5y.SpotifyApi {
     public static class Environment {
         public const string ClientId = "<< ** INPUT YOUR PARAMETOR **>>";
         public const string ClientSecret = "<< ** INPUT YOUR PARAMETOR **>>";
