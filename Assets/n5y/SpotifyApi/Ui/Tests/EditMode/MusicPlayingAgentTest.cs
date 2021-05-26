104c6cda7ff043df2943d2e78447de4e16fa6271
naninunenoy <naninuneno0411@gmail.com>
2021-05-26 23:06:10 +0900

    Testフォルダ追加

---
+++ Assets/n5y/SpotifyApi/Ui/Tests/EditMode/MusicPlayingAgentTest.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using n5y.SpotifyApi.Ui.Core;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace n5y.SpotifyApi.Ui.Tests.EditMode {
+    public class MusicPlayingAgentTest {
+        [Test]
+        public void テスト() {
+            //var agent = new MusicPlayingAgent();
+            Assert.Fail("ToDo");
+        }
+    }
+}
