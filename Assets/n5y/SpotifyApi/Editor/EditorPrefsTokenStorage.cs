55cda763fae0fcbf9291a2b3472f44c56c7e747b
naninunenoy <naninuneno0411@gmail.com>
2021-06-11 23:18:55 +0900

    保存しなくても実行中はrefresh_tokenを保持

--- Assets/n5y/SpotifyApi/Editor/EditorPrefsTokenStorage.cs
+++ Assets/n5y/SpotifyApi/Editor/EditorPrefsTokenStorage.cs
@@ -4,20 +4,26 @@ using Cysharp.Threading.Tasks;
 namespace n5y.SpotifyApi.Editor {
     public class EditorPrefsTokenStorage : IRefreshTokenStorage {
         readonly SpotifyClientSettingsPrefs settings;
+        string temporaryRefreshToken;
 
         public EditorPrefsTokenStorage(SpotifyClientSettingsPrefs settings) {
             this.settings = settings;
+            temporaryRefreshToken = "";
         }
 
         UniTask<string> IRefreshTokenStorage.LoadAsync(CancellationToken cancellationToken) {
             settings.Load();
+            if (string.IsNullOrEmpty(settings.refreshToken.value)) {
+                return UniTask.FromResult(temporaryRefreshToken);
+            }
             return UniTask.FromResult(settings.refreshToken.value);
         }
 
         UniTask IRefreshTokenStorage.SaveAsync(string refreshToken, CancellationToken cancellationToken) {
+            temporaryRefreshToken = refreshToken;
             settings.Load();
-            settings.refreshToken.value = refreshToken;
             if (settings.refreshToken.isSave) {
+                settings.refreshToken.value = refreshToken;
                 settings.Save();
             }
             return UniTask.CompletedTask;
@@ -26,6 +32,7 @@ namespace n5y.SpotifyApi.Editor {
         UniTask IRefreshTokenStorage.DeleteAsync(CancellationToken cancellationToken) {
             settings.Load();
             settings.refreshToken.value = "";
+            temporaryRefreshToken = "";
             settings.Save();
             return UniTask.CompletedTask;
         }
@@ -33,7 +40,8 @@ namespace n5y.SpotifyApi.Editor {
         UniTask<bool> IRefreshTokenStorage.ContainsAsync(CancellationToken cancellationToken) {
             settings.Load();
             var current = settings.refreshToken;
-            return UniTask.FromResult(current.isSave && !string.IsNullOrEmpty(current.value));
+            return UniTask.FromResult(!string.IsNullOrEmpty(current.value) ||
+                                      !string.IsNullOrEmpty(temporaryRefreshToken));
         }
     }
 }
