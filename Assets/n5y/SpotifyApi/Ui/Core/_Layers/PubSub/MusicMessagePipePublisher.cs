72470676247dc64c38a67bc8ebcb19e16c05af89
naninunenoy <naninuneno0411@gmail.com>
2021-06-11 18:28:08 +0900

    pubsubの実装

---
+++ Assets/n5y/SpotifyApi/Ui/Core/_Layers/PubSub/MusicMessagePipePublisher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MessagePipe;
+
+namespace n5y.SpotifyApi.Ui.Core.PubSub {
+    public class MusicMessagePipePublisher : ICurrentMusicPublisher, IMusicCatalogPublisher, IMusicSelectPublisher {
+        public IPublisher<MusicData> NewMusic { get; }
+        public IPublisher<PlaylistTuple> Playlist { get; }
+        public IPublisher<AlbumTuple> Album { get; }
+        public IPublisher<DeviceTuple> Device { get; }
+        public IPublisher<PlaylistMusicTuple> PlaylistMusic { get; }
+        public IPublisher<AlbumMusicTuple> AlbumMusic { get; }
+        public IPublisher<MusicId> MusicSelect { get; }
+        public IPublisher<DeviceId> DeviceSelect { get; }
+
+        public MusicMessagePipePublisher(IServiceProvider resolver) {
+            NewMusic = resolver.GetRequiredService<IPublisher<MusicData>>();
+            Playlist = resolver.GetRequiredService<IPublisher<PlaylistTuple>>();
+            Album = resolver.GetRequiredService<IPublisher<AlbumTuple>>();
+            Device = resolver.GetRequiredService<IPublisher<DeviceTuple>>();
+            PlaylistMusic = resolver.GetRequiredService<IPublisher<PlaylistMusicTuple>>();
+            AlbumMusic = resolver.GetRequiredService<IPublisher<AlbumMusicTuple>>();
+            MusicSelect = resolver.GetRequiredService<IPublisher<MusicId>>();
+            DeviceSelect = resolver.GetRequiredService<IPublisher<DeviceId>>();
+        }
+    }
+}
---
+++ Assets/n5y/SpotifyApi/Ui/Core/_Layers/PubSub/MusicMessagePipeSubscriber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MessagePipe;
+
+namespace n5y.SpotifyApi.Ui.Core.PubSub {
+    public class MusicMessagePipeSubscriber : ICurrentMusicSubscriber, IMusicCatalogSubscriber, IMusicSelectSubscriber{
+        public IBufferedSubscriber<MusicData> Music { get; }
+        public ISubscriber<PlaylistTuple> Playlist { get; }
+        public ISubscriber<AlbumTuple> Album { get; }
+        public ISubscriber<DeviceTuple> Device { get; }
+        public ISubscriber<PlaylistMusicTuple> PlaylistMusic { get; }
+        public ISubscriber<AlbumMusicTuple> AlbumMusic { get; }
+        public ISubscriber<MusicId> MusicSelect { get; }
+        public ISubscriber<DeviceId> DeviceSelect { get; }
+
+        public MusicMessagePipeSubscriber(IServiceProvider resolver) {
+            Music = resolver.GetRequiredService<IBufferedSubscriber<MusicData>>();
+            Playlist = resolver.GetRequiredService<ISubscriber<PlaylistTuple>>();
+            Album = resolver.GetRequiredService<ISubscriber<AlbumTuple>>();
+            Device = resolver.GetRequiredService<ISubscriber<DeviceTuple>>();
+            PlaylistMusic = resolver.GetRequiredService<ISubscriber<PlaylistMusicTuple>>();
+            AlbumMusic = resolver.GetRequiredService<ISubscriber<AlbumMusicTuple>>();
+            MusicSelect = resolver.GetRequiredService<ISubscriber<MusicId>>();
+            DeviceSelect = resolver.GetRequiredService<ISubscriber<DeviceId>>();
+        }
+    }
+}
